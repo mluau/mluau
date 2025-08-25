5c67a667a7f0b4a82bbdaeacb60414458589e078
Your Name <you@example.com>
2025-08-25 22:40:31 +0530

    fix

--- src/state/util.rs
+++ src/state/util.rs
@@ -1,4 +1,5 @@
 use crate::IntoLuaMulti;
+use std::ffi::c_char;
 use std::mem::take;
 use std::os::raw::c_int;
 use std::panic::{catch_unwind, AssertUnwindSafe};
@@ -88,6 +89,7 @@ impl PreallocatedFailure {
 }
 
 unsafe fn push_error_string(state: *mut ffi::lua_State, extra: *mut ExtraData, s: String) {
+    println!("Pushing error string: {s}");
     unsafe fn push_error_string_errorable(
         state: *mut ffi::lua_State,
         extra: *mut ExtraData,
@@ -107,7 +109,8 @@ unsafe fn push_error_string(state: *mut ffi::lua_State, extra: *mut ExtraData, s
     if push_error_string_errorable(state, extra, s).is_err() {
         // If we cannot push the error string, we need to fallback to error userdata
         let s = "memory error".to_string();
-        ffi::lua_pushlstring(state, s.as_ptr() as *const _, s.len());
+        let s_bytes = s.as_bytes();
+        ffi::lua_pushlstring(state, s_bytes.as_ptr() as *const c_char, s_bytes.len());
         drop(s); // Lua copies the string, so we can drop it now
     }
 }
